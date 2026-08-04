84bec2ec593526a45851bd0755af17d0459c54c2
Musa <musaalimasaop@gmail.com>
2026-08-04 11:08:17 +0300

    Steam callback URL duzeltildi

--- app/api/steam/login/route.ts
+++ app/api/steam/login/route.ts
@@ -1,20 +1,16 @@
 import { NextResponse } from "next/server";
 
-
 export async function GET() {
+  const baseUrl = "https://thesuperlig.vercel.app";
 
+  const steamUrl =
+    "https://steamcommunity.com/openid/login?" +
+    "openid.ns=http://specs.openid.net/auth/2.0&" +
+    "openid.mode=checkid_setup&" +
+    `openid.return_to=${encodeURIComponent(baseUrl + "/api/steam/callback")}&` +
+    `openid.realm=${encodeURIComponent(baseUrl)}&` +
+    "openid.identity=http://specs.openid.net/auth/2.0/identifier_select&" +
+    "openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select";
 
-const steamUrl = 
-"https://steamcommunity.com/openid/login?" +
-"openid.ns=http://specs.openid.net/auth/2.0&" +
-"openid.mode=checkid_setup&" +
-"openid.return_to=http://localhost:3000/api/steam/callback&" +
-"openid.realm=http://localhost:3000&" +
-"openid.identity=http://specs.openid.net/auth/2.0/identifier_select&" +
-"openid.claimed_id=http://specs.openid.net/auth/2.0/identifier_select";
-
-
-return NextResponse.redirect(steamUrl);
-
-
+  return NextResponse.redirect(steamUrl);
 }
