47e629e5f45d81f46722f35c8671e572910b4a55
Musa <musaalimasaop@gmail.com>
2026-08-08 12:05:04 +0300

    Build hatası düzeltildi ve Floria güncellendi

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,95 +1,14 @@
 import { prisma } from "../lib/prisma";
 
-
 async function main() {
-
-
-  await prisma.match.createMany({
-
-    data:[
-
-      // 1. Hafta
-
-      {
-        week:1,
-        homeTeam:"Quick Boys",
-        awayTeam:"Floria",
-        homeScore:3,
-        awayScore:1,
-        played:true
-      },
-
-      {
-        week:1,
-        homeTeam:"Nottingham Hotspur",
-        awayTeam:"Panthera FC",
-        homeScore:6,
-        awayScore:0,
-        played:true
-      },
-
-      {
-        week:1,
-        homeTeam:"İnternazionale Milano",
-        awayTeam:"Falcon Buffet",
-        homeScore:3,
-        awayScore:0,
-        played:true
-      },
-
-
-      {
-        week:1,
-        homeTeam:"Ronins",
-        awayTeam:"Real Sociedad",
-        homeScore:2,
-        awayScore:0,
-        played:true
-      },
-
-
-      {
-        week:1,
-        homeTeam:"Mamak FC",
-        awayTeam:"Aedern",
-        homeScore:1,
-        awayScore:8,
-        played:true
-      },
-
-
-      {
-        week:1,
-        homeTeam:"Relentless FC",
-        awayTeam:"West Coast",
-        homeScore:1,
-        awayScore:2,
-        played:true
-      },
-
-
-    ]
-
-  });
-
-
-
-  console.log("Maçlar eklendi");
-
+  console.log("Seed işlemi atlandı.");
 }
 
-
-
 main()
-
-.catch((e)=>{
-
-console.error(e);
-
-})
-
-.finally(async()=>{
-
-await prisma.$disconnect();
-
-});
+  .catch((e) => {
+    console.error(e);
+    process.exit(1);
+  })
+  .finally(async () => {
+    await prisma.$disconnect();
+  });
